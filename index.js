9b7add28a2a3d05376b485b8bf9810564c0a9c5b
jeremy3333 <jeremy.simond@epitech.digital>
2021-03-16 16:48:09 +0100

    adding security

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-const Discord = require('discord.js')
+const { CommandoClient } = require('discord.js-commando');
 const { prefix, token } = require('./config.json');
-const client = new Discord.Client();
+const client = new CommandoClient({
+    commandPrefix: prefix,
+    owner: '189240713249161216'
+});
 
 client.once('ready', () => {
     console.log('Hello World');
